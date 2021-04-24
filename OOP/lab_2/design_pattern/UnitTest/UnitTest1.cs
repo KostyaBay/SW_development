0e9a0de76d6203047c23b8f803567ac5c685c9cb
Kostiantyn Baievskyi <54154093+KostyaBay@users.noreply.github.com>
2021-04-24 17:20:32 +0300

    Update UnitTest1.cs

--- OOP/lab_2/design_pattern/UnitTest/UnitTest1.cs
+++ OOP/lab_2/design_pattern/UnitTest/UnitTest1.cs
@@ -12,7 +12,6 @@ namespace UnitTest
             var actual = warlord.Warlord_easy();
             var expected = "RESULT: Soldier: I obey, sir!";
             Assert.Equal(expected, actual);
-<<<<<<< HEAD
         }
 
         [Fact]
@@ -30,25 +29,6 @@ namespace UnitTest
             var warlord = new Warlord();
             var actual = warlord.Warlord_hard();
             var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!))";    //Warlord: I don't need to check the my subordinates even when giving the hard command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!) + Soldier: I obey, sir!)";     
-=======
-        }
-
-        [Fact]
-        public void TestForCommand_medium()
-        {
-            var program = new Program();
-            var actual = program.Command_medium();
-            var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!)";    //Warlord: Now I've give a medium command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!))  
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void TestForCommand_hard()
-        {
-            var program = new Program();
-            var actual = program.Command_hard();
-            var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!";    //Warlord: I don't need to check the my subordinates even when giving the hard command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!) + Soldier: I obey, sir!)";     
->>>>>>> db33f5844da927964478384c84d5bd9ea48cfae5
             Assert.Equal(expected, actual);
         }
 
