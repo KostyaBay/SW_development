35d9546f2aba28bc2384a350c7c8a35978e1b54b
Kostiantyn Baievskyi <54154093+KostyaBay@users.noreply.github.com>
2021-04-24 15:51:42 +0300

    Update UnitTest1.cs

--- OOP/lab_2/design_pattern/UnitTest/UnitTest1.cs
+++ OOP/lab_2/design_pattern/UnitTest/UnitTest1.cs
@@ -19,7 +19,7 @@ namespace UnitTest
         {
             var program = new Program();
             var actual = program.Command_medium();
-            var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!)";    //\n \n Warlord: Now I've give a medium command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!)) \n \n Warlord: I don't need to check the my subordinates even when giving the hard command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!) + Soldier: I obey, sir!)";     
+            var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!)";    //Warlord: Now I've give a medium command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!))  
             Assert.Equal(expected, actual);
         }
 
@@ -28,7 +28,7 @@ namespace UnitTest
         {
             var program = new Program();
             var actual = program.Command_hard();
-            var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!";    //\n \n Warlord: Now I've give a medium command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!)) \n \n Warlord: I don't need to check the my subordinates even when giving the hard command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!) + Soldier: I obey, sir!)";     
+            var expected = "RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!";    //Warlord: I don't need to check the my subordinates even when giving the hard command: \n RESULT: Transfer(Transfer(Soldier: I obey, sir! + Soldier: I obey, sir!) + Transfer(Soldier: I obey, sir!) + Soldier: I obey, sir!)";     
             Assert.Equal(expected, actual);
         }
 
