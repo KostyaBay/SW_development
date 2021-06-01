a6ba90ed3574176d08869edde3cb8469c5d6d953
Konstantin <konstantin.bay228@gmail.com>
2021-06-02 02:15:29 +0300

    update fp_lab_1

--- OOP/fp_lab_1/src/main/scala/com/const/func_prog/Laba.scala
+++ OOP/fp_lab_1/src/main/scala/com/const/func_prog/Laba.scala
@@ -6,12 +6,6 @@ trait Semigroup[A] {
 }
 
   object Semigroup {
-//    def laws[A: Arbitrary : Shrink](implicit sg: Semigroup[A], p: A => Pretty) = {
-//      List(
-//        forAll { (a: A, b: A, c: A) => sg.combine(sg.combine(a, b), c) == sg.combine(a, sg.combine(b, c)) }, //associative
-//        forAll { (a: A, b: A) => sg.combine(a, b) == sg.combine(b, a) } //commutative
-//      )
-//    }
   implicit val BigDecimalSemigroup = new Semigroup[BigDecimal] {
     override def combine(x: BigDecimal, y: BigDecimal): BigDecimal = new BigDecimal(x.bigDecimal.add(y.bigDecimal), x.mc)
   }
@@ -19,9 +13,5 @@ trait Semigroup[A] {
 
 object Laba {
   def main(args: Array[String]): Unit = {
-    //Semigroup.laws[BigDecimal].foreach(_.check())
-    //val sg = Semigroup.BigDecimalSemigroup
-    //forAll { (a: BigDecimal, b: BigDecimal, c: BigDecimal) => sg.combine(me.combine(a, b), c) == sg.combine(a, sg.combine(b, c)) }.check()
-    //forAll { (a: BigDecimal, b: BigDecimal) => sg.combine(a,b) == sg.combine(b,a) }.check()
   }
 }
